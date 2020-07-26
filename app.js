a178be067c77ba46d2804a1212be6c4fcfed0fb8
MuhammadOwais786 <62741318+MuhammadOwais786@users.noreply.github.com>
2020-07-26 10:03:55 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
 function addItem() {
     //Step 1: Get Input value
     var inputElement = document.getElementById('input')
+
     //Step 2.1: Create a <li> element & attach input value
     var listItem = document.createElement('LI')
+    listItem.setAttribute('class','task')
     listItem.innerHTML = "<span>" + inputElement.value + "</span>"
+    
     //Step 2.2: Create a delete <button> element & attach it to <li>
-    // var deleteButton = document.createElement('BUTTON')
-    var deleteButton = document.createElement('I')
-    // deleteButton.innerHTML = "Delete"
-    deleteButton.innerHTML = ""
-    deleteButton.setAttribute('onclick', 'deleteItem(event)','class', 'fa fa-trash-alt')
-    deleteButton.setAttribute('class', 'fa fa-trash-alt')
+    var deleteButton = document.createElement('BUTTON')
+    // var deleteButton = document.createElement('IMG')
+    
+    deleteButton.innerHTML = "Delete"
+    deleteButton.setAttribute('onclick', 'deleteItem(event)')
+    deleteButton.setAttribute('src', 'image.png')
+    // deleteButton.setAttribute('width', '20px')
+    // deleteButton.setAttribute('height', '30px')
+    deleteButton.setAttribute('class', 'delete')
+    
+    
+    // deleteButton.setAttribute('class', 'fa fa-trash-alt')
     listItem.appendChild(deleteButton)
+    
     //Step 2.3: Create a edit <button> element & attach it to <li>
     var editButton = document.createElement('BUTTON')
+    // var editButton = document.createElement('IMG')
     editButton.innerHTML = "Edit"
+    
     // editButton.innerHTML = ""
     editButton.setAttribute('onclick', 'editItem(event)')
+    // editButton.setAttribute('src', 'pencil.png')
+    // editButton.setAttribute('width', '20px')
+    // editButton.setAttribute('height', '30px')
+    editButton.setAttribute('class', 'edit')
+
     // deleteButton.setAttribute('class', 'fa fa-pencil')
     listItem.appendChild(editButton)
+    
     //Step 3: Attach this element to HTML
     var list = document.getElementById('list')
     list.appendChild(listItem)
+    
     //Step 4: Empty Input Value
     inputElement.value = ""
 
